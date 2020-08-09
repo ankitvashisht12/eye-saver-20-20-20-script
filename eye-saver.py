e08f276c4dbc96f0e956e91c0be09655665b2a67
Ankit Vashisht <ankitvashisht12@gmail.com>
2020-08-09 19:31:50 +0530

    fix minute conversion

--- eye-saver.py
+++ eye-saver.py
@@ -3,14 +3,14 @@ import time
 from playsound import playsound # pip install playsound
 
 
-WORK_TIME_MIN = 20
-REST_TIME_SEC = 30
+WORK_TIME_MIN = 20.0
+REST_TIME_SEC = 30.0
 
 
 start = time.time()
 
 while True:
-	if (time.time() - start)*60 == WORK_TIME_MIN:
+	if (time.time() - start)/60 == WORK_TIME_MIN:
 		playsound( os.path.join('sounds', 'ding.mp3'))
 		time.sleep(REST_TIME_SEC)
 		playsound( os.path.join('sounds', 'end.mp3'))
