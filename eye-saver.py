e22b3337849c51cc95cee45a69cbc028c02290ed
Ankit Vashisht <ankitvashisht12@gmail.com>
2020-08-09 10:50:59 +0530

    code for 20 Min

--- eye-saver.py
+++ eye-saver.py
@@ -10,7 +10,7 @@ REST_TIME_SEC = 20
 start = time.time()
 
 while True:
-	if (time.time() - start) == WORK_TIME_MIN:
+	if (time.time() - start)*60 == WORK_TIME_MIN:
 		playsound('ding.mp3')
 		time.sleep(REST_TIME_SEC)
 		playsound('end.mp3')
